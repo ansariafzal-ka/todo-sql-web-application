0db4e257642ee767d3b2efa71b0b228cbe9b24ab
ansariafzal <ansariafzal2112@gmail.com>
2024-08-29 21:48:32 +0530

    test route added

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/ping", (request, response) => {
+  response.send("This is a test route");
+});
+
 app.use("/", todoRouter);
 
 const PORT = process.env.PORT || 5000;
